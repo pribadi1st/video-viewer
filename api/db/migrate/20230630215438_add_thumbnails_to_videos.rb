c3822b6e68f0b7d426015414cdcf8132c7948620
pribadi1st <pribadiridwan@rocketmail.com>
2023-07-01 11:15:03 +0200

    Fix ignorization

--- api/db/migrate/20230630215438_add_thumbnails_to_videos.rb
+++
@@ -1,7 +0,0 @@
-class AddThumbnailsToVideos < ActiveRecord::Migration[6.1]
-  def change
-    add_column :videos, :thumb_small, :string
-    add_column :videos, :thumb_medium, :string
-    add_column :videos, :thumb_large, :string
-  end
-end
