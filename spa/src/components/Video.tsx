e4f723c6dd233bce62771c7efd7643cc691138fa
pribadi1st <pribadiridwan@rocketmail.com>
2023-07-02 20:04:04 +0200

    Show and hide title

--- spa/src/components/Video.tsx
+++ spa/src/components/Video.tsx
@@ -17,9 +17,17 @@ interface VideoProps {
 
 const Video = (props:VideoProps) => {
   const { url, title, thumbnails, category } = props;
+  const [show, setShow] = React.useState(false);
+  const showTitle = (state: boolean = false) => {
+    setShow(state)
+  }
 
   return (
-    <div className="card video">
+    <div
+      className="card video"
+      onMouseEnter={() => showTitle(true)}
+      onMouseLeave={() => showTitle(false)}
+    >
       <video
         controls
         src={url}
@@ -28,6 +36,14 @@ const Video = (props:VideoProps) => {
         className="video-player"
         poster={thumbnails.large}
       />
+      {
+        show && 
+          <div className="position-absolute top-0 start-0 bg-light w-100">
+            <p className="mt-3">
+              {title}
+            </p>
+          </div>
+      }
 
       <div className="card-body">
         {category.text}
